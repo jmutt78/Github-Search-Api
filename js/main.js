90f04a187e406e38f480438f3d6c5c67f3a3dd79
jmutt78 <jmcintosh@docusites.com>
2018-10-08 12:58:46 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  console.log('ready');
+});
+
+//Event listerner that gets users submission//
+function inputSubmit(){
+
+
+}
+
+//Ajax request to get object information
+function getGetHubData(){
+
+
+}
+//dispalys thumbnail and title
+function dataDisplay(){
+
+}
+
+//function handler
+function handleGethubApi(){
+
+
+}
+
+$(handleGethubApi);
