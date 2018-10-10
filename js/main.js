63a5d14655a8d50e000223673754f59dac95e0fc
jmutt78 <jmcintosh@docusites.com>
2018-10-09 18:39:18 -0700

    config api

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-$(document).ready(function(){
-  console.log('ready');
-});
+'use strict'
+
+const apiKey =
+
+const searchUrl = `https://api.github.com/users/jmutt78/repos`;
 
 //Event listerner that gets users submission//
 function inputSubmit(){
-
+$('#search-bar').on('keyup', function(e){
+  let searchQeury = e.target.value;
+});
 
 }
 
 //Ajax request to get object information
 function getGetHubData(){
 
+
+  fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 
 }
 //dispalys thumbnail and title
@@ -20,7 +36,7 @@ function dataDisplay(){
 
 //function handler
 function handleGethubApi(){
-
+inputSubmit();
 
 }
 
