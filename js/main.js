3bba9ca4686dafd731b745d9b40b0374008330da
jmutt78 <jmcintosh@docusites.com>
2018-10-10 14:44:16 -0700

    last

--- js/main.js
+++ js/main.js
@@ -13,9 +13,9 @@ function renderResult(result) {
   return `
     <div>
       <h2>
-      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
-      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
-      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+      <p>Number of watchers: <span class="watchers-count">${result.watchers_count}</span></p>
+      <p>Number of open issues: <span class="issues-count">${result.open_issues}</span></p>
     </div>
   `;
 }
